477457b94cc938cb543a77647433fd4b347da92b
David Anthoff <anthoff@berkeley.edu>
2021-04-03 17:55:56 -0700

    Set more narrow file perms

--- build.jl
+++ build.jl
@@ -4,7 +4,7 @@ using URIParser, FilePaths
 
 pkgname = "ElectronJS"
 version = v"9.1.0"
-build = 5
+build = 6
 
 build_path = joinpath(@__DIR__, "build")
 
@@ -95,7 +95,7 @@ mktempdir() do temp_path
                 for (root, dirs, files) in walkdir(artifact_dir) 
                     cd(root) do
                         for file in files
-                            run(`chmod +x "$file"`)
+                            run(`chmod u+x "$file"`)
                         end
                     end
                 end
